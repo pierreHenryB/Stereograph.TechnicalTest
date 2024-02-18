06f1e81189973685ca2799f2461ab7314911ef1e
pierrehenryB <pierrehenry.bartholome@gmail.com>
2024-02-18 15:02:18 +0100

    create empty crud controller

--- Stereograph.TechnicalTest.Api/Controllers/PersonController.cs
+++ Stereograph.TechnicalTest.Api/Controllers/PersonController.cs
@@ -1,9 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace Stereograph.TechnicalTest.Api.Controllers;
 
-namespace Stereograph.TechnicalTest.Api.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 [Route("api/persons")]
 [ApiController]
 public class PersonController : ControllerBase
 {
+
+    public PersonController()
+    {
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IList<Contracts.Person>>> GetPersonsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost()]
+    public async Task<ActionResult<Contracts.Person>> AddPersonAsync(Contracts.Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut()]
+    public async Task<ActionResult<Contracts.Person>> UpdatePersonAsync(Contracts.Person person)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete()]
+    public async Task<ActionResult<int>> DeletePersonAsync(Contracts.Person person)
+    {
+        throw new NotImplementedException();
+    }
 }
