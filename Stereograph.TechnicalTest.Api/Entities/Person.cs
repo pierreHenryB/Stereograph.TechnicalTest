b9acacb6f59668ba587dbf6533bc6fd62069610f
pierrehenryB <pierrehenry.bartholome@gmail.com>
2024-02-18 14:36:45 +0100

    Ajout du contrat et de l'entité pour une Person Autorisation des nullables dans la solution

---
+++ Stereograph.TechnicalTest.Api/Entities/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace Stereograph.TechnicalTest.Api.Entities;
+
+/// <summary>
+/// Entity of <see cref="Person"/>.
+/// </summary>
+public class Person
+{
+    /// <summary>
+    /// Identifier.
+    /// </summary>
+    public int? Id { get; set; }
+
+    /// <summary>
+    /// FirstName.
+    /// </summary>
+    public string? FirstName { get; set; }
+
+    /// <summary>
+    /// LastName.
+    /// </summary>
+    public string? LastName { get; set; }
+
+    /// <summary>
+    /// Email.
+    /// </summary>
+    public string? Email { get; set; }
+
+    /// <summary>
+    /// Address.
+    /// </summary>
+    public string? Address { get; set; }
+}
