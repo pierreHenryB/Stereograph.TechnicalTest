e1250f33c56aece556e715175f2e7e0a05dc63c5
pierrehenryB <pierrehenry.bartholome@gmail.com>
2024-02-18 16:01:08 +0100

    Ajout mapping csv file Ajout mapping automapper

---
+++ Stereograph.TechnicalTest.Api/Mapping/Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Stereograph.TechnicalTest.Api.Mapping;
+
+using Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extensions class
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// Add mapping services
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection"/></param>
+    public static void AddMapping(this IServiceCollection services)
+    {
+        services.AddAutoMapper(typeof(PersonProfile));
+    }
+}
---
+++ Stereograph.TechnicalTest.Api/Mapping/PersonMapCsv.cs
@@ -0,0 +1,20 @@
+﻿namespace Stereograph.TechnicalTest.Api.Mapping;
+
+using CsvHelper.Configuration;
+
+/// <summary>
+/// Class that define the mapping of csv for <see cref="Entities.Person"/>
+/// </summary>
+public class PersonMapCsv : ClassMap<Entities.Person>
+{
+    /// <summary>
+    /// Constructor by default
+    /// </summary>
+    public PersonMapCsv()
+    {
+        Map(m => m.FirstName).Name("first_name");
+        Map(m => m.LastName).Name("last_name");
+        Map(m => m.Email).Name("email");
+        Map(m => m.Address).Name("address");
+    }
+}
